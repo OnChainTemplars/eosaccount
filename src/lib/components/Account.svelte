80b9465d1fb997888a5490fc725048fcf8d7a703
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2025-11-17 23:07:18 +0700

    Added components

---
+++ src/lib/components/Account.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<div class="account">
+    <div>
+        <h3>
+            {data.title}
+        </h3>
+        <div class="avatar">
+            <img alt="Avatar" src={data.avatar} />
+        </div>
+        <div>
+            <p>{data.desc}</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .avatar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+        max-width: 300px;
+        max-height: 300px;
+        overflow: hidden;
+    }
+
+    .avatar img {
+        max-height: 300px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    import { getAccountInfo } from "$lib/index"
+    import { getAccountInfo } from "$lib/index";
+    import Account from "$lib/components/Account.svelte";
 
-    const accountInfo = "account.info"
+    const accountInfo = "account.info";
 
-    let account = $state(accountInfo)
-    let promise = $state(getAccountInfo(accountInfo))
+    let account = $state(accountInfo);
+    let promise = $state(getAccountInfo(accountInfo));
 </script>
 
 <div class="block">
@@ -13,57 +14,33 @@
         <input
             placeholder="Search EOS Account"
             bind:value={account}
-            onchange={() => promise = getAccountInfo(account)}
-        >
+            onchange={() => (promise = getAccountInfo(account))}
+        />
     </div>
     <div>
         {#await promise}
             <div>...</div>
-        {:then data} 
-            <div>
-                <h3>
-                    {data.title}
-                </h3>
-            </div>
-            <div class="avatar">
-                <img alt="Avatar" src={data.avatar}>
-            </div>
-            <div>
-                <p>{data.desc}</p>
-            </div>
+        {:then data}
+            <Account {data} />
         {/await}
     </div>
 </div>
 
 <style>
-.block {
-    margin-left: auto;
-    margin-right: auto;
-    background-color: white;
-    max-width: 400px;
-    padding: 5px 15px;
-    border-radius: 10px;
-    text-align: center;
-}
+    .block {
+        margin-left: auto;
+        margin-right: auto;
+        background-color: white;
+        max-width: 400px;
+        padding: 5px 15px;
+        border-radius: 10px;
+        text-align: center;
+    }
 
-.block input {
-    background: #F3F6F4;
-    padding: 5px;
-    border: 0;
-    border-radius: 10px;
-}
-
-.avatar {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    max-width: 300px;
-    max-height: 300px;
-    overflow: hidden;
-}
-
-.avatar img {
-    max-height: 300px;
-}
+    .block input {
+        background: #f3f6f4;
+        padding: 5px;
+        border: 0;
+        border-radius: 10px;
+    }
 </style>
