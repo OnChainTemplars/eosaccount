b5468184f90f813b890b9913ae1f1b1ad254433b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2025-11-17 23:08:01 +0700

    Refactoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
@@ -10,7 +10,9 @@
 	<style>
 		html {
 			background-color: ghostwhite;
-			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, Fira, Sans, "Droid Sans", "Helvetica Neue", sans-serif;
+			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+				Oxygen, Ubuntu, Cantarell, Fira, Sans, "Droid Sans",
+				"Helvetica Neue", sans-serif;
 		}
 	</style>
 </svelte:head>
